ba5f435fd185881a88e91cd1487a8158ee10e1a4
Neil Vallecer <neilvallecer12@gmail.com>
2025-07-19 13:42:54 +0800

    feat: implemented nanoid for user IDs - added nanoid as a new dependency - updated table to have varchar 12 instead of autoincrement int

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,8 +1,16 @@
-import { int, mysqlTable, serial, varchar, timestamp } from 'drizzle-orm/mysql-core';
+import { mysqlTable, varchar, timestamp } from 'drizzle-orm/mysql-core';
 import { createSelectSchema } from 'drizzle-zod';
+import { nanoid } from 'nanoid';
+
+// const customId = (length = 12): string => {
+//   const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
+//   let result = ''
+//   for (let i = 0; i < length; i++) result += chars.charAt(Math.floor(Math.random() * chars.length))
+//   return result
+// }
 
 export const users = mysqlTable('users', {
-  id: int().primaryKey().autoincrement(),
+  id: varchar({ length: 12 }).primaryKey().$default(() => nanoid(12)),
   email: varchar({ length: 255 }).notNull().unique(),
   password_hash: varchar({ length: 255 }).notNull(),
   first_name: varchar({ length: 100 }).notNull(),
