53d1ff7f5136dd18bb614bfab582303100d25501
Neil Vallecer <neilvallecer12@gmail.com>
2025-07-26 10:25:50 +0800

    refactor:  modified if statement to directly check the environement variable if it's empty since the neon instantiation always returns a value even if the database url doesn't exist

--- src/db/index.ts
+++ src/db/index.ts
@@ -3,12 +3,10 @@ import { neon } from '@neondatabase/serverless'
 import { drizzle } from 'drizzle-orm/neon-http'
 
 export function createDb(c: Context) {
-  const connection = neon(c.env.DATABASE_URL as string)
-
-  if (!connection)
+  if (!c.env.DATABASE_URL)
     throw new Error('Database URL is required')
 
-  const db = drizzle({ client: connection })
+  const db = drizzle({ client: neon(c.env.DATABASE_URL as string) })
 
   return db
 }
