5111c0282f50c4986a673ff7b68bcb5aa124690d
Stanleigh Morales <stanleighmorales@gmail.com>
2025-08-07 20:42:47 +0800

    Merge branch 'develop' of https://github.com/StanleighMorales/Lab-System-Backend into Secure-Token-Storage-httpOnly-Cookies-Implementation

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -3,28 +3,21 @@ import { createMiddleware } from 'hono/factory'
 import { verify } from 'hono/jwt'
 import type { AppBindings } from '@/lib/types/app-types'
 import { HTTPException } from 'hono/http-exception'
+import { getCookie } from 'hono/cookie'
 
 export const authMiddleware = createMiddleware<AppBindings>(async (c, next) => {
-  //Authorization header
-  const authHeader = c.req.header('Authorization')
+  const token = getCookie(c, 'accessToken')
 
-  // Check if the header exists and is in the correct "Bearer" format
-  if (!authHeader || !authHeader.startsWith('Bearer ')) {
-    throw new HTTPException(401, { message: 'Unauthorized: Missing or invalid token' })
+  if (!token) {
+    throw new HTTPException(401, { message: 'Unauthorized: Missing access token' })
   }
-
-  //Extract token string
-  const token = authHeader.substring(7) // "Bearer ".length is 7
 
   try {
-    // Verify the token using your secret.
     const payload = await verify(token, c.env.JWT_SECRET)
-
-    //Set the verified payload on the context for the next handler.
     c.set('jwtPayload', payload)
-
   } catch (error) {
-    throw new HTTPException(401, { message: 'Unauthorized: Invalid token' })
+    throw new HTTPException(401, { message: 'Unauthorized: Invalid access token' })
   }
+
   await next()
 })
