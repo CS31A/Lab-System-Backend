e145cf51b958180d3e8b29352f06060efca1add7
Stanleigh Morales <stanleighmorales@gmail.com>
2025-08-12 06:19:43 +0800

    refactor(auth): change error handling to use UNAUTHORIZED status for missing or invalid access token

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -10,14 +10,14 @@ export const authMiddleware = createMiddleware<AppBindings>(async (c, next) => {
   const token = getCookie(c, 'accessToken')
 
   if (!token) {
-    throw new HTTPException(httpStatusCodes.FORBIDDEN, { message: 'Unauthorized: Missing access token' })
+    throw new HTTPException(httpStatusCodes.UNAUTHORIZED, { message: 'Unauthorized: Missing access token' })
   }
 
   try {
     const payload = await verify(token, c.env.JWT_SECRET)
     c.set('jwtPayload', payload)
   } catch (error) {
-    throw new HTTPException(httpStatusCodes.FORBIDDEN, { message: 'Unauthorized: Invalid access token' })
+    throw new HTTPException(httpStatusCodes.UNAUTHORIZED, { message: 'Unauthorized: Invalid access token' })
   }
 
   await next()
