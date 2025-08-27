209a73d36baf5a27c795f9f671e6e8a87997c0f0
Neil Vallecer <neilvallecer12@gmail.com>
2025-08-27 15:16:00 +0800

    minor: added test for the index and health check endpoints

---
+++ tests/handlers/index.spec.ts
@@ -0,0 +1,31 @@
+import { beforeEach, describe, expect, it, vi } from 'vitest'
+import app from '@/index'
+
+const MOCK_ENV = {
+  LOG_LEVEL: 'silent',
+  DB_URL: 'postgresql://neondb_owner:npg_nwdvIUp6hg4b@ep-flat-sun-a1s7no3s-pooler.ap-southeast-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require',
+  NODE_ENV: 'development',
+  JWT_SECRET: 'supersecretidoldexiarongtestingdomini',
+}
+
+describe('health Check', () => {
+  it('gET /health', async () => {
+    const res = await app.request('/health', {}, MOCK_ENV)
+    expect(res.status).toBe(200)
+
+    const data: { message: string, status: string, timestamp: any } = await res.json()
+
+    expect(data.message).toBe('Server is running')
+    expect(data.status).toBe('healthy')
+    expect(data).toHaveProperty('timestamp')
+  })
+})
+
+describe('root path', () => {
+  it('get /', async () => {
+    const res = await app.request('/', {}, MOCK_ENV)
+    expect(res.status).toBe(200)
+    const data: { message: string } = await res.json()
+    expect(data.message).toBe('Hono API')
+  })
+})
