545ee941cb7c7e9a6a1758ad8f812cd08fab2ac3
Stanleigh Morales <stanleighmorales@gmail.com>
2025-09-05 16:43:54 +0800

    refactor: update user routes to use specific path for middleware

--- src/routes/users/users.index.ts
+++ src/routes/users/users.index.ts
@@ -18,15 +18,15 @@ import * as routes from '@/routes/users/users.route'
  * We then export this router to be registered in the root index.ts file
  */
 const router = createRouter()
-router.use('/users/*', authMiddleware)
-
-router.use('/users/*', requireRole(['admin']))
+router.use('/users', authMiddleware)
+// Admin only routes
+router.use('/users', requireRole(['admin']))
 router.openapi(routes.createUserRoute, createHandlers.CreateUserHandler)
 router.openapi(routes.updateUserRoute, updateHandlers.UpdateUserHandler)
 router.openapi(routes.softDeleteUserRoute, SoftDeleteUserHandler)
 router.openapi(routes.restoreUserRoute, RestoreUserHandler)
-
-router.use('/users/*', requireRole(['admin', 'teacher', 'technical']))
+// Admin, Teacher, Technical routes
+router.use('/users', requireRole(['admin', 'teacher', 'technical']))
 router.openapi(routes.getUserRoute, GetUserHandler)
 router.openapi(routes.getAllUsersRoute, GetAllUsersHandler)
 router.openapi(routes.listUsersRoute, ListUsersHandler)
